3762be85bdb2a1fab6144ad93a1bb0d32a1fc483
chienminh21 <hhffgg436@gmail.com>
2026-01-28 04:34:10 +0700

    h

--- phiphaimain.lua
+++ phiphaimain.lua
@@ -3,11 +3,18 @@ local player = Players.LocalPlayer
 
 local discordInvite = "https://discord.gg/6y7TpYTBkW"
 
+-- COPY CLIPBOARD (executor only)
+if setclipboard then
+	setclipboard(discordInvite)
+elseif toclipboard then
+	toclipboard(discordInvite)
+end
+
 task.wait(1)
 
 player:Kick(
-	"/INDO/ Tolong copy script terbaru di Discord\n" ..
-	"/AR/ يرجى نسخ السكربت الجديد في Discord\n" ..
-	"/EN/ Please copy the new script in Discord\n\n" ..
+	"/INDO/ Link Discord đã được copy\n" ..
+	"/AR/ تم نسخ رابط Discord\n" ..
+	"/EN/ Discord link copied\n\n" ..
 	discordInvite
 )
