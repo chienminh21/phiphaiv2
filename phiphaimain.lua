5c23224838e7e1c650e4e3e2d0e65e816f0ed89f
chienminh21 <hhffgg436@gmail.com>
2026-02-10 22:53:31 +0700

    h

--- phiphaimain.lua
+++ phiphaimain.lua
@@ -5,6 +5,7 @@ local Scripts = {
 	[136764190843219]  = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/knockout.lua",
 	[137826330724902] = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/LobbyScaryShawarmaKiosk.lua",
     [128001665358186] = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/ScaryShawarmaKiosk.lua",
+	[133862653684768] = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/knockout.lua",
 }
 
 local url = Scripts[PlaceId]
