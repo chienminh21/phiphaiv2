1fbf7ee65c58a88ef313f60da075feb594fc83a4
chienminh21 <hhffgg436@gmail.com>
2026-02-10 19:13:42 +0700

    h

--- phiphaimain.lua
+++ phiphaimain.lua
@@ -2,7 +2,7 @@ local PlaceId = game.PlaceId
 
 local Scripts = {
 	[119524908037342] = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/LobbyBaksoMalangAnomalies.lua",
-	[77338972879392]  = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/BaksoMalangAnomalies.lua",
+	[136764190843219]  = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/knockout.lua",
 	[137826330724902] = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/LobbyScaryShawarmaKiosk.lua",
     [128001665358186] = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/ScaryShawarmaKiosk.lua",
 }
