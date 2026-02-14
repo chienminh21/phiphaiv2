1eb449558f8aeb1fe3a4c3c966dfe3e487935c3a
chienminh21 <hhffgg436@gmail.com>
2026-02-14 23:31:20 +0700

    h

--- phiphaimain.lua
+++ phiphaimain.lua
@@ -1,16 +1,1 @@
-local PlaceId = game.PlaceId
-
-local Scripts = {
-	[102212685525266] = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/shotbrainrot.lua",
-	[136764190843219]  = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/knockout.lua",
-	[137826330724902] = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/LobbyScaryShawarmaKiosk.lua",
-    [128001665358186] = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/ScaryShawarmaKiosk.lua",
-	[133862653684768] = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/knockout.lua",
-}
-
-local url = Scripts[PlaceId]
-if url then
-	loadstring(game:HttpGet(url))()
-else
-	warn("Game No Support")
-end
+loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/d36e215c2e0c4047"))()
