e761cbde9c3c3fa2c96e003fe1d5c21eb4a9ffc0
chienminh21 <hhffgg436@gmail.com>
2026-02-23 23:59:44 +0700

    Update phiphaimain.lua

--- phiphaimain.lua
+++ phiphaimain.lua
@@ -2,7 +2,7 @@ local PlaceId = game.PlaceId
 
 local Scripts = {
 	[102212685525266] = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/shotbrainrot.lua",
-	[]  = "",
+	[130594398886540]  = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/gardenhorizons",
 	[137826330724902] = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/LobbyScaryShawarmaKiosk.lua",
   [128001665358186] = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/ScaryShawarmaKiosk.lua",
 	[] = "",
