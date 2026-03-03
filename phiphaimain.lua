3926352fb6cb84d42a5e6379a8dae99652765e01
chienminh21 <hhffgg436@gmail.com>
2026-03-03 19:32:00 +0700

    h

--- phiphaimain.lua
+++ phiphaimain.lua
@@ -9,6 +9,7 @@ local Scripts = {
   [116495829188952] = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/DeadRails.lua",
   [70876832253163] = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/DeadRails.lua",
 	[108172906114565] = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/sol%E2%80%99sRNGBoss2.lua",
+	[103854444055060] = "https://raw.githubusercontent.com/chienminh21/phiphaiv2/refs/heads/main.lua/SilentAssassins.lua",
 }
 
 local url = Scripts[PlaceId]
