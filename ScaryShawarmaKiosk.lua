4ef7f031b27946bc859dab5d7122c61697d7bf8f
chienminh21 <hhffgg436@gmail.com>
2026-05-01 00:39:10 +0700

    Update ScaryShawarmaKiosk.lua

--- ScaryShawarmaKiosk.lua
+++ ScaryShawarmaKiosk.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/chienminh21/aaaaaaaa/refs/heads/main/helpdevhuhu.lua"))()
-loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/5a2ab009bcf84f25"))()
+loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/e2725819923b4def"))()
