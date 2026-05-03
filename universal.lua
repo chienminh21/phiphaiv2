c6ac8b04dd95cf64978a602aa7a071768e962eae
chienminh21 <hhffgg436@gmail.com>
2026-05-03 07:36:51 +0700

    Update universal.lua

--- universal.lua
+++ universal.lua
@@ -1,1 +1,10 @@
 
+local function N(t)
+    pcall(function()
+        game.StarterGui:SetCore("SendNotification",{Title="PhiPhai v2",Text=t,Duration=5})
+    end)
+end
+
+task.delay(2,function()
+    N("commingsoon")
+end)
