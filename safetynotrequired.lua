dadea544e73b9051dc0de38e7c149de4e53cfe10
chienminh21 <hhffgg436@gmail.com>
2026-05-13 12:22:12 +0700

    Update safetynotrequired.lua

--- safetynotrequired.lua
+++ safetynotrequired.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/46b6540699094f4e"))()
+loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/3f49bb831ee041b9"))()
